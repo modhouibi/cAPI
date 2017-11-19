163be5cfb1ea8bf8190a3a849226a848f6f66d5e
modhouibi <m.dhouibi.dev@gmail.com>
2017-11-19 02:13:46 +0000

    mongodb err

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,6 +6,6 @@ var Promise=mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:270
 ,{useMongoClient:true});
 
 Promise.then(db=>console.log('connected'))
-.catch(err=>{throw err});
+.catch(err=>{console.log('UNABLE TO CONNECT to DB!!'); throw err;});
 
 module.exports={mongoose};
