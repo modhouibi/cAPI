d0e5dbde5bf53b8c9066acc49683a70ad701ce25
modhouibi <m.dhouibi.dev@gmail.com>
2017-11-19 02:07:51 +0000

    mongo cennection error handler

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ mongoose.Promise=global.Promise;
 var Promise=mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'
 ,{useMongoClient:true});
 
-Promise.then(db=>console.log('connected'),
-err=>console.log(err));
+Promise.then(db=>console.log('connected'))
+.catch(err=>{throw err});
 
 module.exports={mongoose};
